a6d1e4646c6acf19b773459216e6812346254d4d
likerhythm <wjdalsdl0412@gmail.com>
2026-09-07 11:02:49 +0900

    프로젝트 세팅

---
+++ p04-concurrent-leaderboard/src/main/java/org/example/p04concurrentleaderboard/P04ConcurrentLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package org.example.p04concurrentleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P04ConcurrentLeaderboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P04ConcurrentLeaderboardApplication.class, args);
+    }
+
+}
---
+++ p04-concurrent-leaderboard/src/test/java/org/example/p04concurrentleaderboard/P04ConcurrentLeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.p04concurrentleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P04ConcurrentLeaderboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
